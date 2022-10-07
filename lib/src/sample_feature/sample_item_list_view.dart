9a0100690f2d43789dfa9a5766fcdea4ac7135bc
Martin Jainta <martin.jainta@gmail.com>
2022-10-07 12:09:26 +0200

    Display recipe information

--- lib/src/sample_feature/sample_item_list_view.dart
+++ lib/src/sample_feature/sample_item_list_view.dart
@@ -5,18 +5,23 @@ import '../settings/settings_view.dart';
 import '../settings/settings_controller.dart';
 import 'sample_item.dart';
 import 'sample_item_details_view.dart';
+import '../services/repository.dart';
+import '../classes/recipe.dart';
 
 class SampleItemListView extends StatefulWidget {
-  const SampleItemListView(
-      {super.key,
-      this.items = const [SampleItem(1), SampleItem(2), SampleItem(3)],
-      required this.settingsController});
+  const SampleItemListView({
+    super.key,
+    this.items = const [],
+    required this.settingsController,
+    required this.repository,
+  });
 
   final SettingsController settingsController;
+  final Repository repository;
 
   static const routeName = '/';
 
-  final List<SampleItem> items;
+  final List<RecordModel> items;
 
   @override
   _State createState() => _State();
@@ -25,59 +30,68 @@ class SampleItemListView extends StatefulWidget {
 class _State extends State<SampleItemListView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sample Items'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.settings),
-            onPressed: () async {
-              final client = PocketBase(widget.settingsController.pbBaseUrl);
-              await client.users.authViaEmail(widget.settingsController.pbMail,
-                  widget.settingsController.pbPass);
-              var records = await client.records.getFullList('pizza_recipes');
-              print(records);
-              // Navigate to the settings page. If the user leaves and returns
-              // to the app after it has been killed while running in the
-              // background, the navigation stack is restored.
-              Navigator.restorablePushNamed(context, SettingsView.routeName);
-            },
-          ),
-        ],
-      ),
-
-      // To work with lists that may contain a large number of items, it’s best
-      // to use the ListView.builder constructor.
-      //
-      // In contrast to the default ListView constructor, which requires
-      // building all Widgets up front, the ListView.builder constructor lazily
-      // builds Widgets as they’re scrolled into view.
-      body: ListView.builder(
-        // Providing a restorationId allows the ListView to restore the
-        // scroll position when a user leaves and returns to the app after it
-        // has been killed while running in the background.
-        restorationId: 'sampleItemListView',
-        itemCount: widget.items.length,
-        itemBuilder: (BuildContext context, int index) {
-          final item = widget.items[index];
-
-          return ListTile(
-              title: Text('SampleItem ${item.id}'),
-              leading: const CircleAvatar(
-                // Display the Flutter Logo image asset.
-                foregroundImage: AssetImage('assets/images/flutter_logo.png'),
+    List<RecordModel>? myItems = [];
+    return FutureBuilder(
+        future: widget.repository.getRecipes(),
+        builder: (context, AsyncSnapshot<List<RecordModel>> snapshot) {
+          if (snapshot.hasData) {
+            myItems = snapshot.data;
+            return Scaffold(
+              appBar: AppBar(
+                title: const Text('Recipes'),
+                actions: [
+                  IconButton(
+                    icon: const Icon(Icons.settings),
+                    onPressed: () async {
+                      // Navigate to the settings page. If the user leaves and returns
+                      // to the app after it has been killed while running in the
+                      // background, the navigation stack is restored.
+                      Navigator.restorablePushNamed(
+                          context, SettingsView.routeName);
+                    },
+                  ),
+                ],
               ),
-              onTap: () {
-                // Navigate to the details page. If the user leaves and returns to
-                // the app after it has been killed while running in the
-                // background, the navigation stack is restored.
-                Navigator.restorablePushNamed(
-                  context,
-                  SampleItemDetailsView.routeName,
-                );
-              });
-        },
-      ),
-    );
+
+              // To work with lists that may contain a large number of items, it’s best
+              // to use the ListView.builder constructor.
+              //
+              // In contrast to the default ListView constructor, which requires
+              // building all Widgets up front, the ListView.builder constructor lazily
+              // builds Widgets as they’re scrolled into view.
+              body: ListView.builder(
+                // Providing a restorationId allows the ListView to restore the
+                // scroll position when a user leaves and returns to the app after it
+                // has been killed while running in the background.
+                restorationId: 'sampleItemListView',
+                itemCount: myItems!.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final item = myItems![index];
+                  final recipe = Recipe.fromRecord(item);
+
+                  return ListTile(
+                      title: Text(recipe.name),
+                      leading: const CircleAvatar(
+                        // Display the Flutter Logo image asset.
+                        foregroundImage:
+                            AssetImage('assets/images/flutter_logo.png'),
+                      ),
+                      trailing: Text('For ${recipe.ballNo} dough balls'),
+                      onTap: () {
+                        // Navigate to the details page. If the user leaves and returns to
+                        // the app after it has been killed while running in the
+                        // background, the navigation stack is restored.
+                        Navigator.restorablePushNamed(
+                          context,
+                          SampleItemDetailsView.routeName,
+                        );
+                      });
+                },
+              ),
+            );
+          } else {
+            return CircularProgressIndicator();
+          }
+        });
   }
 }
