1ce682bd0c6a30beecac4a223be49d7fc8847d43
Martin Jainta <martin.jainta@gmail.com>
2022-10-09 17:28:39 +0200

    Add floating action button
    Reformat using Android Studio

--- lib/src/sample_feature/sample_item_list_view.dart
+++ lib/src/sample_feature/sample_item_list_view.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:pocketbase/pocketbase.dart';
 
-import '../settings/settings_view.dart';
-import '../settings/settings_controller.dart';
-import 'sample_item.dart';
-import 'sample_item_details_view.dart';
-import '../services/repository.dart';
 import '../classes/recipe.dart';
+import '../services/repository.dart';
+import '../settings/settings_controller.dart';
+import '../settings/settings_view.dart';
+import 'sample_item_details_view.dart';
 
 class SampleItemListView extends StatefulWidget {
   const SampleItemListView({
@@ -32,65 +31,73 @@ class _State extends State<SampleItemListView> {
   Widget build(BuildContext context) {
     List<Recipe>? myItems = [];
     return FutureBuilder(
-        future: widget.repository.getRecipes(),
-        builder: (context, AsyncSnapshot<List<Recipe>> snapshot) {
-          if (snapshot.hasData) {
-            myItems = snapshot.data;
-            return Scaffold(
-              appBar: AppBar(
-                title: const Text('Recipes'),
-                actions: [
-                  IconButton(
-                    icon: const Icon(Icons.settings),
-                    onPressed: () async {
-                      // Navigate to the settings page. If the user leaves and returns
-                      // to the app after it has been killed while running in the
-                      // background, the navigation stack is restored.
-                      Navigator.restorablePushNamed(
-                          context, SettingsView.routeName);
-                    },
+      future: widget.repository.getRecipes(),
+      builder: (context, AsyncSnapshot<List<Recipe>> snapshot) {
+        if (snapshot.hasData) {
+          myItems = snapshot.data;
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Recipes'),
+              actions: [
+                IconButton(
+                  icon: const Icon(Icons.settings),
+                  onPressed: () async {
+                    // Navigate to the settings page. If the user leaves and returns
+                    // to the app after it has been killed while running in the
+                    // background, the navigation stack is restored.
+                    Navigator.restorablePushNamed(
+                        context, SettingsView.routeName);
+                  },
+                ),
+              ],
+            ),
+
+            // To work with lists that may contain a large number of items, it’s best
+            // to use the ListView.builder constructor.
+            //
+            // In contrast to the default ListView constructor, which requires
+            // building all Widgets up front, the ListView.builder constructor lazily
+            // builds Widgets as they’re scrolled into view.
+            body: ListView.builder(
+              // Providing a restorationId allows the ListView to restore the
+              // scroll position when a user leaves and returns to the app after it
+              // has been killed while running in the background.
+              restorationId: 'sampleItemListView',
+              itemCount: myItems!.length,
+              itemBuilder: (BuildContext context, int index) {
+                final recipe = myItems![index];
+
+                return ListTile(
+                  title: Text(recipe.name),
+                  leading: const CircleAvatar(
+                    // Display the Flutter Logo image asset.
+                    foregroundImage:
+                        AssetImage('assets/images/flutter_logo.png'),
                   ),
-                ],
-              ),
-
-              // To work with lists that may contain a large number of items, it’s best
-              // to use the ListView.builder constructor.
-              //
-              // In contrast to the default ListView constructor, which requires
-              // building all Widgets up front, the ListView.builder constructor lazily
-              // builds Widgets as they’re scrolled into view.
-              body: ListView.builder(
-                // Providing a restorationId allows the ListView to restore the
-                // scroll position when a user leaves and returns to the app after it
-                // has been killed while running in the background.
-                restorationId: 'sampleItemListView',
-                itemCount: myItems!.length,
-                itemBuilder: (BuildContext context, int index) {
-                  final recipe = myItems![index];
-
-                  return ListTile(
-                      title: Text(recipe.name),
-                      leading: const CircleAvatar(
-                        // Display the Flutter Logo image asset.
-                        foregroundImage:
-                            AssetImage('assets/images/flutter_logo.png'),
-                      ),
-                      trailing: Text('For ${recipe.ballNo} dough balls'),
-                      onTap: () {
-                        // Navigate to the details page. If the user leaves and returns to
-                        // the app after it has been killed while running in the
-                        // background, the navigation stack is restored.
-                        Navigator.restorablePushNamed(
-                          context,
-                          SampleItemDetailsView.routeName,
-                        );
-                      });
-                },
-              ),
-            );
-          } else {
-            return CircularProgressIndicator();
-          }
-        });
+                  trailing: Text('For ${recipe.ballNo} dough balls'),
+                  onTap: () {
+                    // Navigate to the details page. If the user leaves and returns to
+                    // the app after it has been killed while running in the
+                    // background, the navigation stack is restored.
+                    Navigator.restorablePushNamed(
+                      context,
+                      SampleItemDetailsView.routeName,
+                    );
+                  },
+                );
+              },
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () {
+                print('Switch to "new recipe" feature');
+              },
+              child: const Icon(Icons.add),
+            ),
+          );
+        } else {
+          return CircularProgressIndicator();
+        }
+      },
+    );
   }
 }
