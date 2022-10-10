d36499ccd600b922d91a97b430f75fb3bd6880e3
Martin Jainta <martin.jainta@gmail.com>
2022-10-10 21:42:07 +0200

    Improve layout and switch honey/yeast to salt/oil for dough ingredients

--- lib/src/features/recipes/recipe_add_view.dart
+++ lib/src/features/recipes/recipe_add_view.dart
@@ -22,82 +22,161 @@ class RecipeAddView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Recipe Details'),
       ),
-      body: FormBuilder(
-        key: formKey,
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Expanded(
-                  child: FormBuilderTextField(
-                    name: 'recipe_name',
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter a recipe name';
-                      }
-                      return null;
-                    },
-                    decoration: const InputDecoration(
-                      labelText: 'Recipe Name',
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: FormBuilder(
+          key: formKey,
+          child: Column(
+            children: [
+              FormBuilderTextField(
+                name: 'recipe_name',
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a recipe name';
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Recipe Name',
+                ),
+              ),
+              const Divider(
+                thickness: 0,
+              ),
+              FormBuilderSlider(
+                name: 'recipe_ball_no',
+                min: 1,
+                max: 10,
+                initialValue: 4,
+                divisions: 9,
+                decoration: const InputDecoration(
+                  labelText: 'Ball number',
+                ),
+              ),
+              const Divider(
+                height: 40,
+                thickness: 0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: Column(
+                      children: [
+                        FormBuilderTextField(
+                          name: 'recipe_poolish_flour',
+                          decoration: const InputDecoration(
+                            labelText: 'Poolish Flour',
+                          ),
+                        ),
+                        FormBuilderTextField(
+                          name: 'recipe_poolish_water',
+                          decoration: const InputDecoration(
+                            labelText: 'Poolish Water',
+                          ),
+                        ),
+                        FormBuilderTextField(
+                          name: 'recipe_poolish_honey',
+                          decoration: const InputDecoration(
+                            labelText: 'Poolish Honey',
+                          ),
+                        ),
+                        FormBuilderTextField(
+                          name: 'recipe_poolish_yeast',
+                          decoration: const InputDecoration(
+                            labelText: 'Poolish Yeast',
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                ),
-                Expanded(
-                  child: FormBuilderSlider(
-                    name: 'recipe_ball_no',
-                    min: 1,
-                    max: 10,
-                    initialValue: 4,
-                    divisions: 9,
-                    decoration: const InputDecoration(
-                      labelText: 'Ball number',
+                  const VerticalDivider(),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        FormBuilderTextField(
+                          name: 'recipe_dough_flour',
+                          decoration: const InputDecoration(
+                            labelText: 'Dough Flour',
+                          ),
+                        ),
+                        FormBuilderTextField(
+                          name: 'recipe_dough_water',
+                          decoration: const InputDecoration(
+                            labelText: 'Dough Water',
+                          ),
+                        ),
+                        FormBuilderTextField(
+                          name: 'recipe_dough_salt',
+                          decoration: const InputDecoration(
+                            labelText: 'Dough Salt',
+                          ),
+                        ),
+                        FormBuilderTextField(
+                          name: 'recipe_dough_oil',
+                          decoration: const InputDecoration(
+                            labelText: 'Dough Oil',
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                ),
-              ],
-            ),
-            const Divider(),
-            FormBuilderTextField(
-              name: 'recipe_poolish_ingredients',
-              decoration: const InputDecoration(
-                labelText: 'Poolish Ingredients',
+                ],
               ),
-            ),
-            FormBuilderTextField(
-              name: 'recipe_dough_ingredients',
-              decoration: const InputDecoration(
-                labelText: 'Dough Ingredients',
+              const Divider(
+                height: 20,
+                thickness: 0,
               ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                if (formKey.currentState!.validate()) {
-                  String name =
-                      formKey.currentState?.fields['recipe_name']?.value;
-                  int ballNo = formKey
-                      .currentState?.fields['recipe_ball_no']?.value
-                      .toInt();
-                  //Map<String, dynamic> poolishIngredients = json.decode(formKey
-                  //  .currentState
-                  //    ?.fields['recipe_poolish_ingredients']
-                  //      ?.value);
-                  //Map<String, dynamic> doughIngredients = json.decode(formKey.currentState?.fields['recipe_dough_ingredients']?.value);
-                  Map<String, dynamic> poolishIngredients = {
-                    "flour": "300gr 00",
-                  };
-                  Map<String, dynamic> doughIngredients = {
-                    "flour": "300gr 00",
-                    "water": "300ml",
-                  };
-                  var recipe = Recipe.fromValues(
-                      name, ballNo, poolishIngredients, doughIngredients);
-                  repository.addRecipe(recipe);
-                }
-              },
-              child: const Text('Save Recipe'),
-            ),
-          ],
+              ElevatedButton(
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    String name =
+                        formKey.currentState?.fields['recipe_name']?.value;
+                    int ballNo = formKey
+                        .currentState?.fields['recipe_ball_no']?.value
+                        .toInt();
+                    //Map<String, dynamic> poolishIngredients = json.decode(formKey
+                    //  .currentState
+                    //    ?.fields['recipe_poolish_ingredients']
+                    //      ?.value);
+                    //Map<String, dynamic> doughIngredients = json.decode(formKey.currentState?.fields['recipe_dough_ingredients']?.value);
+                    Map<String, dynamic> poolishIngredients = {
+                      "flour": formKey.currentState
+                              ?.fields['recipe_poolish_flour']?.value ??
+                          '',
+                      "water": formKey.currentState
+                              ?.fields['recipe_poolish_water']?.value ??
+                          '',
+                      "honey": formKey.currentState
+                              ?.fields['recipe_poolish_honey']?.value ??
+                          '',
+                      "yeast": formKey.currentState
+                              ?.fields['recipe_poolish_yeast']?.value ??
+                          '',
+                    };
+                    Map<String, dynamic> doughIngredients = {
+                      "flour": formKey.currentState
+                              ?.fields['recipe_dough_flour']?.value ??
+                          '',
+                      "water": formKey.currentState
+                              ?.fields['recipe_dough_water']?.value ??
+                          '',
+                      "salt": formKey.currentState?.fields['recipe_dough_salt']
+                              ?.value ??
+                          '',
+                      "oil": formKey.currentState?.fields['recipe_dough_oil']
+                              ?.value ??
+                          '',
+                    };
+                    var recipe = Recipe.fromValues(
+                        name, ballNo, poolishIngredients, doughIngredients);
+                    repository.addRecipe(recipe);
+                  }
+                },
+                child: const Text('Save Recipe'),
+              ),
+            ],
+          ),
         ),
       ),
     );
